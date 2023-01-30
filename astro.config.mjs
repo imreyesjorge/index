a957a399c5e26dac7163c2b6ba6e6a64a3bf673a
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2023-01-30 16:47:31 -0600

    add prefetch to blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://reyes.cool",
+  integrations: [prefetch()]
 });
