a581cba68123a740007fc42c05e6efe680f9e9da
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2023-12-09 22:50:45 -0600

    Update nextjs version

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,20 +4,20 @@ import { getPost } from "./post.logic";
 export default async function PostScreen({ params }) {
   const post: Post = await getPost(params.slug);
 
-  return (
-    post && (
-      <main>
-        <section className="blog_post">
-          <div className="blog_post_info">
-            <h1>{post?.title}</h1>
-            <small>{post?.readTimeInMinutes} minutes</small>
-          </div>
-          <div
-            className="blog_post_content"
-            dangerouslySetInnerHTML={{ __html: post.content.html }}
-          ></div>
-        </section>
-      </main>
-    )
+  return post ? (
+    <main>
+      <section className="blog_post">
+        <div className="blog_post_info">
+          <h1>{post?.title}</h1>
+          <small>{post?.readTimeInMinutes} minutes</small>
+        </div>
+        <div
+          className="blog_post_content"
+          dangerouslySetInnerHTML={{ __html: post.content.html }}
+        ></div>
+      </section>
+    </main>
+  ) : (
+    <p>{params.slug}</p>
   );
 }
