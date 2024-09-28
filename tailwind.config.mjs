ea276eaffd62e79ccb28d62d8316fd672f0c1133
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2024-09-27 18:20:56 -0600

    add mono-spaced font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'sans': ['Inter Variable', 'ui-sans-serif', 'system-ui'],
-				'serif': ['Alegreya Variable','ui-serif', 'Georgia'],
-				'mono': ['ui-monospace', 'SFMono-Regular'],
-				'display': ['Oswald'],
-				'body': ['"Open Sans"'],
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Variable", "ui-sans-serif", "system-ui"],
+        serif: ["Alegreya Variable", "ui-serif", "Georgia"],
+        mono: ["Fira Code Variable", "ui-monospace", "SFMono-Regular"],
+        display: ["Oswald"],
+        body: ['"Open Sans"'],
+      },
+    },
+  },
+  plugins: [],
+};
