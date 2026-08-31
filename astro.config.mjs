e92c1271465f1eddf2000db2be56b0f16e3b20db
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2026-08-31 01:03:11 -0600

    update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
-import vercel from '@astrojs/vercel/serverless';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   output: 'server',
-  adapter: vercel({})
+  adapter: vercel({}),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
